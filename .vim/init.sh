2235b58d301facf45188e9320a4ba593aaac7d11
Jake Luer <jake@alogicalparadox.com>
2013-08-12 17:27:01 +0300

    vim: add pathogen and vim airline

--- .vim/init.sh
+++ .vim/init.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+VIM_INIT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+apt-get install -y vim
+
+cd ~
+ln -sf $VIM_INIT_DIR/.vimrc .vimrc
+ln -sf $VIM_INIT_DIR/lib .vim
+
+echo "Vim installed."
